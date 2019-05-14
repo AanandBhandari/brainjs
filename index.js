b1593b643ce2f10fdc66e001357ee4c54dafd707
AanandBhandari <aanandbhandari143@gmail.com>
2019-05-14 11:55:51 +0545

    prediciting restaurant name

--- index.js
+++ index.js
@@ -1,69 +1,47 @@
-// input: { red, green, blue }
-// ouput: { light, neutral, dark }
+const restaurants = {
+    "global": "Sunday",
+    "Brilliant Yellow Corral": "Monday",
+    "Penny’s": "Tuesday",
+    "bells cafe": "Thursday",
+    "Right Coast Wings": "Wednesday",
+    "The Delusion Last Railway Car": "Thursday",
+    "Fun Day Inn": "Friday",
+    "JHOP": "Saturday",
+    "Owls": "Sunday",
+    "kfc": "Thursday"
+};
 
-const colors = [
-    { green: 0.2, blue: 0.4 },
-    { green: 0.4, blue: 0.6 },
-    { red: 0.2, green: 0.8, blue: 0.8 },
-    { green: 1, blue: 1 },
-    { red: 0.8, green: 1, blue: 1 },
-    { red: 1, green: 1, blue: 1 },
-    { red: 1, green: 0.8, blue: 0.8 },
-    { red: 1, green: 0.6, blue: 0.6 },
-    { red: 1, green: 0.4, blue: 0.4 },
-    { red: 1, green: 0.31, blue: 0.31 },
-    { red: 0.8 },
-    { red: 0.6, green: 0.2, blue: 0.2 }
-];
-
-const brightnesses = [
-    { dark: 0.8 },
-    { neutral: 0.8 },
-    { light: 0.7 },
-    { light: 0.8 },
-    { light: 0.9 },
-    { light: 1 },
-    { light: 0.8 },
-    { neutral: 0.7, light: 0.5 },
-    { dark: 0.5, neutral: 0.5 },
-    { dark: 0.6, neutral: 0.3 },
-    { dark: 0.85 },
-    { dark: 0.9 }
-];
+// input: { Monday, Tuesday, Wednesday, etc. }
+// output: { Restaurant1, Restaurant2 }
 
 const trainingData = [];
 
-for (let i = 0; i < colors.length; i++) {
+for (restaurantName in restaurants) {
+    const dayOfWeek = restaurants[restaurantName]
     trainingData.push({
-        input: colors[i],
-        output: brightnesses[i]
+        input : {[dayOfWeek]:1},
+        output :{[restaurantName]: 1}
     });
 }
-
+// console.log(trainingData);
 const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
 
 const stats = net.train(trainingData);
+// console.log(stats);
+// console.log(net.run({ 'Monday': 1 }));
 
-console.log(stats);
-
-console.log(net.run({
-    red: 0.9
-}));
-// input: { light, neutral, dark }
-// output: { red, green, blue }
-
-const invertedTrainingData = [];
-
-for (let i = 0; i < colors.length; i++) {
-    invertedTrainingData.push({
-        input: brightnesses[i],
-        output: colors[i]
-    });
+// function returning the restaurantForDay name
+function restaurantForDay(dayOfWeek){
+    const result = net.run({[dayOfWeek]: 1 });
+    console.log(result);
+    let highestValue = 0;
+    let highestResturentName = '';
+    for(let restaurantName in result) {
+        if (result[restaurantName]>highestValue) {
+            highestValue = result[restaurantName]
+            highestResturentName = restaurantName
+        }
+    }
+    return highestResturentName
 }
-
-const invertedNet = new brain.NeuralNetwork({ hiddenLayers: [3] });
-
-const invertedStats = invertedNet.train(invertedTrainingData);
-
-console.log(invertedStats);
-
+console.log(restaurantForDay('Sunday'));
