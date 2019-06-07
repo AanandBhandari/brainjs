13c3c3e185d964f215a15b43ccf83e74a3d47cb2
AanandBhandari <aanandbhandari143@gmail.com>
2019-06-07 23:18:59 +0545

    semantic dectection

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const trainingData = [
-    'Jane saw Doug.',
-    'Doug saw Jane.',
-    'Spot saw Doug and Jane looking at each other.',
-    'It was love at first sight, and Spot had a frontrow seat. It was a very special moment for all.'
+    { input: 'I am super happy!', output: 'happy' },
+    { input: 'What a pill!', output: 'sarcastic' },
+    { input: 'I am super unhappy!', output: 'sad' },
+    { input: 'Are we there yet?', output: 'excited' }
 ];
 
 const net = new brain.recurrent.LSTM();
 net.train(trainingData, {
-    iterations: 1500,
-    errorThresh: 0.011
+    iterations: 100,
+    erroThresh: 0.011
 });
 
-console.log(net.run('Jane'));
-console.log(net.run('It was'));
+console.log(net.run('I am unhappy!'));
+console.log(net.run('I am happy!'));
